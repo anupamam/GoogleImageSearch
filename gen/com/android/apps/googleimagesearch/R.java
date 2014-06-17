a4d33b4d662f1f28226893883decacb5543f70e2
anupamam <anupamamann07@gmail.com>
2014-06-16 19:08:39 -0700

    conflict resolve

--- gen/com/android/apps/googleimagesearch/R.java
+++ gen/com/android/apps/googleimagesearch/R.java
@@ -8,19 +8,15 @@
 package com.android.apps.googleimagesearch;
 
 public final class R {
-<<<<<<< HEAD
     public static final class array {
         public static final int icon_size_array=0x7f050000;
         public static final int image_color_array=0x7f050002;
         public static final int image_type_array=0x7f050001;
     }
-=======
->>>>>>> 3e3baec031574fd583dfc4f3718e678e0f46dcfc
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-<<<<<<< HEAD
         public static final int ic_menu=0x7f020001;
         public static final int ic_search=0x7f020002;
         public static final int ic_settings=0x7f020003;
@@ -68,33 +64,6 @@ public final class R {
         public static final int title_activity_image_display=0x7f040003;
         public static final int title_activity_preferences=0x7f040004;
         public static final int type_prompt=0x7f04000d;
-=======
-        public static final int ic_search=0x7f020001;
-        public static final int ic_settings=0x7f020002;
-        public static final int ic_xoom=0x7f020003;
-    }
-    public static final class id {
-        public static final int btnSearch=0x7f070002;
-        public static final int etQuery=0x7f070001;
-        public static final int frameLayout1=0x7f070003;
-        public static final int gvImages=0x7f070004;
-        public static final int ivResult=0x7f070000;
-        public static final int miSettings=0x7f070005;
-    }
-    public static final class layout {
-        public static final int activity_image_display=0x7f030000;
-        public static final int activity_search=0x7f030001;
-        public static final int item_image_result=0x7f030002;
-    }
-    public static final class menu {
-        public static final int menu_preferences=0x7f060000;
-    }
-    public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int query_hint=0x7f040002;
-        public static final int title_activity_image_display=0x7f040003;
->>>>>>> 3e3baec031574fd583dfc4f3718e678e0f46dcfc
     }
     public static final class style {
         /** 
@@ -118,18 +87,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-<<<<<<< HEAD
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
-=======
-        public static final int AppBaseTheme=0x7f050000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f050001;
->>>>>>> 3e3baec031574fd583dfc4f3718e678e0f46dcfc
     }
 }
